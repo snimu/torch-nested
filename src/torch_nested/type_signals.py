269f62d2b96f1fc03e3bd8702b88b9a1c297a4cb
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-01-20 17:06:11 +0100

    Change docstrings in type_signals.py

--- src/torch_nested/type_signals.py
+++ src/torch_nested/type_signals.py
@@ -13,14 +13,14 @@ from typing import Any
 @dataclass
 class AccessTensorsAttr:
     """
-    Signal to `Tensors.__getitem__` that the `data.tensors`-attribute
+    Signal to `NestedTensors.__getitem__` that the `data.tensors`-attribute
     should be accessed instead of `data[...]`.
     """
 
 
 @dataclass
 class ObjectWithTensorsAttr:
-    """Allow `Shape` to represent objects with `tensor`-attribute."""
+    """Allow `NestedShape` to represent objects with `tensor`-attribute."""
 
     name: str
     _size: Any | None
